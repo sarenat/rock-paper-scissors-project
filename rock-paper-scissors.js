8736543116fd268b07a637acb1fd9d83e9273945
sarenat <sarenattran@gmail.com>
2025-08-11 15:55:06 -0400

    Implement logic for playing one round
    Used switch case to determine round winner as well as updated
    scores for each user.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -18,11 +18,70 @@ function getHumanChoice() {
 const computerChoice = getComputerChoice();
 const humanChoice = getHumanChoice();
 
+// DEBUGGING CONSOLE.LOGS
+console.log("humanChoice: ", humanChoice);
+console.log("computerChoice: ", computerChoice);
+
 // Initialize computer score and human score
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    humanChoice.toLowerCase();
-    computerChoice.toLowerCase();
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    let winnerPrompt = "";
+
+    switch(humanChoice) {
+        case "rock":
+            if (computerChoice == "scissors") {
+                winnerPrompt = "You win! Rock beats scissors.";
+                humanScore++;
+                break;
+            }
+            else if (computerChoice == "paper") {
+                winnerPrompt = "You lose! Paper beats rock.";
+                computerScore++;
+                break;
+            }
+            else {
+                winnerPrompt = "It's a tie!";
+                break;
+            }
+        case "scissors":
+            if (computerChoice == "rock") {
+                winnerPrompt = "You lose! Rock beats scissors.";
+                computerScore++;
+                break;
+            }
+            else if (computerChoice == "paper") {
+                winnerPrompt = "You win! Scissors beats paper.";
+                humanScore++;
+                break;
+            }
+            else {
+                winnerPrompt = "It's a tie!";
+                break;
+            }
+        case "paper":
+            if (computerChoice == "scissors") {
+                winnerPrompt = "You lose! Scissors beats paper.";
+                computerScore++;
+                break;
+            }
+            else if (computerChoice == "rock") {
+                winnerPrompt = "You win! Paper beats rock.";
+                humanScore++;
+                break;
+            }
+            else {
+                winnerPrompt = "It's a tie!";
+                break;
+            }
+    }
+    console.log("winnerPrompt: ", winnerPrompt);
+    console.log("humanScore: ", humanScore);
+    console.log("computerScore: ", computerScore);
 }
+
+playRound(humanChoice, computerChoice);
